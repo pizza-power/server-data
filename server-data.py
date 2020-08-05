8671df8e0937f8939430434fabbf2b7962828360
pizza-power <pizzapwr@gmail.com>
2020-08-05 16:27:00 -0400

    refactor variable names, change to exception handling, config file changes

--- server-data.py
+++ server-data.py
@@ -5,47 +5,51 @@ from subprocess import PIPE, run
 import configparser
 from datetime import date
 
-DATE =  str(date.today())
-logfile = open("log.txt", "a")
+DATE = str(date.today())
+LOGFILE = open("config.txt", "a")
+
 
 def send(data_to_send: str):
     try:
         try:
             creds = configparser.ConfigParser()
-            creds.read("creds.ini")
+            creds.read("config.ini")
             username = creds.get("creds", "username")
             password = creds.get("creds", "password")
+            recipient = creds.get("recipient", "recipient")
+
+            try:
+                subject = "Server Info " + DATE
+                body = data_to_send
+                final_message = 'Subject: {}\n\n{}'.format(subject, body)
+                server = smtplib.SMTP('smtp.gmail.com', 587)
+                server.starttls()
+                server.login(username, password)
+                server.sendmail(username, recipient, final_message)
+                LOGFILE.write(str(date.today()) + " report success\n")
+                LOGFILE.close()
+
+            except smtplib.SMTPException:
+                LOGFILE.write(str(date.today()) + "email error\n")
+
         except configparser.Error:
-            # TODO write to log that no config found, abort
-            logfile.write(DATE + " creds not found\n")
-            logfile.close()
+            LOGFILE.write(DATE + " creds not found\n")
+            LOGFILE.close()
 
-        to = "pizzapwr@gmail.com"
-        subject = "Server Info " + DATE
-        body = data_to_send
-
-        final_message = 'Subject: {}\n\n{}'.format(subject, body)
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(username, password)
-        server.sendmail(username, to, final_message)
-        logfile.write(str(date.today()) + " success\n")
-        logfile.close()
     except:
-        logfile.write(str(date.today()) + " failure\n")
-        logfile.close()
+        LOGFILE.write(str(date.today()) + " generic failure\n")
+        LOGFILE.close()
 
 
-def out(command_to_execute: str):
+def out(command_to_execute: str) -> str:
     result = run(command_to_execute, stdout=PIPE, stderr=PIPE,
                  universal_newlines=True, shell=True)
     return result.stdout
 
-
+# build list of commands to email, add commands to data[] as needed
 data = ""
-
-commands = ["uptime", "users", "df -h", "cat /proc/mdstat", "apcaccess"]
-
+commands = ["uptime", "users", "last -10", "df -h", "cat /proc/mdstat",
+            "apcaccess"]
 for command in commands:
     data += "\n" + out(command)
 
